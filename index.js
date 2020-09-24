978ab40144e2dd345726cd6222a5019db95d9b7f
shvm61 <60419370+shvm61@users.noreply.github.com>
2020-09-24 20:18:38 +0530

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 require("dotenv").config();
 // const db = require("./config/mongoose");
 const passport = require("passport");
